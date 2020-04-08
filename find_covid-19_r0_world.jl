82874f9cd19b7f2dada17e022054d7e3e0d61c28
Bill <wherrera10@users.noreply.github.com>
2020-04-07 19:49:35 -1000

    add plotting

--- find_covid-19_r0_world.jl
+++ find_covid-19_r0_world.jl
@@ -1,15 +1,13 @@
-# Simplistic Julia extrapolation model to fit world data totals
-
-using LsqFit
+using LsqFit, Plots, Dates, Formatting, Measures
+plotly()
 
 const K = 7_800_000_000  # approximate world population
 const n0 = 27  # starting at day 0 with 27 Chinese cases
 
 """ The model for logistic regression with a given r0 """
-@. model(t, r0) = (n0 * exp(r0 * t)) / (( 1 + n0 * (exp(r0 * t) - 1) / K))
+@. model(t, r) = (n0 * exp(r * t)) / (( 1 + n0 * (exp(r * t) - 1) / K))
 
-# Source: https://ourworldindata.org/coronavirus#all-charts-preview
-# Daily world totals, including China and all countries
+# Daily world totals of covid cases, all countries
 ydata = [
 27, 27, 27, 44, 44, 59, 59, 59, 59, 59, 59, 59, 59, 60, 60,
 61, 61, 66, 83, 219, 239, 392, 534, 631, 897, 1350, 2023,
@@ -24,19 +22,34 @@ ydata = [
 656866, 715353, 777796, 851308, 928436, 1000249, 1082054,
 1174652,
 ]
-# The initial data is for 97 days starting with day 0 = 31 December 2019
 tdata = collect(LinRange(0.0, 96, 97))
 
-# starting approximation for r0 of 1/2
-r0param = [0.5]
+# starting approximation for r of 1/2
+rparam = [0.5]
 
-fit = curve_fit(model, tdata, ydata, r0param)
+fit = curve_fit(model, tdata, ydata, rparam)
 
-# Our answer for r0 given the world data and simplistic model
-R0 = fit.param
-println("The R0 for the world data is: ", R0)
+# Our answer for r given the world data and simplistic model
+r = fit.param
+println("The logistic curve r for the world data is: ", r)
 println("The confidence interval at 5% significance is: ",
     confidence_interval(fit, 0.05))
+println("Since R0 ≈ exp(G * r), and G ≈ 7 days, R0 ≈ ", exp(7r[1]))
+#=
+The logistic curve r for the world data is: [0.11230217572265622]
+The confidence interval at 5% significance is: [(0.11199074156706985, 0.11261360987824258)]
+Since R0 ≈ exp(G * r), and G ≈ 7 days, R0 ≈ 2.1948533427511663
+=#
 
-# output --> The R0 for the world data is: [0.11230217572265622]
+x = collect(LinRange(0.0, 366, 367))
+y = model(x, r)
+ydelta = [0.0; [y[i] - y[i-1] for i in 2:367]]
+mx, idx = findmax(ydelta)
+maxinc, mday = Int(round(mx)), Date(2019, 12, 31) + Day(idx)
+plt = plot(x, y, color=:blue, xlabel="Days in 2020", right_margin=20mm,
+    ylabel="Persons Potentially Infected", label="Infected")
+plot!(x, ydelta, color=:red, label="Newly Infected",
+    title="Max increase is $(format(maxinc, commas=true)) on $mday")
+display(plt)
+readline()
 
